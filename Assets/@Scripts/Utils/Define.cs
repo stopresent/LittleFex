cb9d3787de6c6d259bd0d312df8532fa91cc9134
Stopresent <86364202+stopresent@users.noreply.github.com>
2023-09-12 13:42:33 +0900

    Rookiss류 템플릿 생성 및 한글 깨짐 방지 처리

---
+++ Assets/@Scripts/Utils/Define.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum Scene
+    {
+        Unknown,
+        TitleScene,
+        LobbyScene,
+        GameScene,
+    }
+
+    public enum Sound
+    {
+        Bgm,
+        SubBgm,
+        Effect,
+        Max,
+    }
+
+    public enum UIEvent
+    {
+        Click,
+        Preseed,
+        PointerDown,
+        PointerUp,
+        BeginDrag,
+        Drag,
+        EndDrag,
+    }
+
+}
